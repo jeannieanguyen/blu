af0a7e45f465a628f6cff9375d5e1341172ddcfc
Jeannie Nguyen <jeannie.jujubee@gmail.com>
2018-12-11 11:10:57 -0800

    Add all the amplify stuff

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import logo from "./logo.svg";
 import "./App.css";
 import Home from "./components/home";
@@ -6,9 +7,11 @@ import Home from "./components/home";
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Home />
-      </div>
+      <Router>
+        <div className="App">
+          <Route exact path="/" component={Home} />
+        </div>
+      </Router>
     );
   }
 }
