b7dbf896162fb30906b7afe46779d5603da69a13
Jeannie Nguyen <jeannie.jujubee@gmail.com>
2018-12-11 10:26:53 -0800

    Add home screen

---
+++ src/components/blob.js
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+
+export default class Blob extends Component {
+  render() {
+    return (
+      <svg
+        id="organic-blob"
+        width="300"
+        height="300"
+        xmlns="http://www.w3.org/2000/svg"
+        filter="url(#goo)"
+      >
+        <defs>
+          <filter id="goo">
+            <feGaussianBlur
+              in="SourceGraphic"
+              stdDeviation="10"
+              result="blur"
+            />
+            <feColorMatrix
+              in="blur"
+              mode="matrix"
+              values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 19 -9"
+              result="goo"
+            />
+            <feComposite in="SourceGraphic" in2="goo" operator="atop" />
+          </filter>
+        </defs>
+        <circle id="Circle1" />
+        <circle id="Circle2" />
+        <circle id="Circle3" />
+        <circle id="Circle4" />
+      </svg>
+    );
+  }
+}
---
+++ src/components/home.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import Blob from "./blob";
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div className="home">
+        <Blob />
+        <h1>Blü</h1>
+      </div>
+    );
+  }
+}
